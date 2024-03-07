a718e40a2b84533ee5ef0f3c5e6814c88fa2d807
Pascal7601 <levispascal63@gmail.com>
2024-03-07 13:26:12 -0800

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
     def __init__(self):
         """initializing attributes of BaseModel class"""
         self.id = uuid.uuid4().hex
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+
+    def __str__(self):
+        """returns a string of the class"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+
+    def save(self):
+        """updates the instance updated_at"""
+        self.updated_at = datetime.now()
+
+
+    def to_dict(self):
+        """returns a dictionary containig key value of instance in __dict__"""
+        obj_dict = self.__dict__.copy
+        obj_dict['__class__'] = self.__class__.__name__
+
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+
+        return obj_dict
 
 
 User_id = BaseModel()
-print(User_id.id)
-
-
+print(User_id)
