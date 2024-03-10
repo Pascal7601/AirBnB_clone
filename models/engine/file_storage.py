39e5181cea5c0614048d7a01531fa110c7d54315
papuskoo <kelvinnayman@gmail.com>
2024-03-10 08:51:11 +0300

    Improve documentation for BaseModel class1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,27 +1,54 @@
 #!/usr/bin/python3
+"""
+Module: file_storage
+
+This module defines the FileStorage class, which provides functionality to serialize instances to a JSON file and deserialize JSON file to instances.
+"""
+
 import json
 
 
 class FileStorage:
-    """serializes instances to JSON file and deserializes JSON file to instances"""
+    """
+    FileStorage class serializes instances to a JSON file and deserializes JSON file to instances.
+
+    Attributes:
+        __file_path (str): The path to the JSON file used for serialization.
+        __objects (dict): A dictionary containing serialized instances.
+    """
 
     __file_path = "file.json"
     __objects = {}
 
 
     def all(self):
-        """returns the dictionary __objects"""
+        """
+        Retrieves all serialized instances.
+
+        Returns:
+            dict: A dictionary containing all serialized instances.
+        """
         return self.__objects
 
 
     def new(self, obj):
-        """sets objects """
+        """
+        Adds a new object to the __objects dictionary.
+
+        Args:
+            obj: The object to be added.
+        """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
 
     def save(self):
-        """serializes objects to JSON file"""
+        """
+        Serializes objects to the JSON file.
+
+        Note:
+            This method serializes all objects stored in __objects dictionary to the JSON file specified by __file_path attribute.
+        """
         all_obj = self.__objects
         serial_obj = {}
         for key, obj in all_obj.items():
@@ -32,10 +59,16 @@ class FileStorage:
 
     
     def reload(self):
+        """
+        Deserializes JSON file to load objects into memory.
+
+        Note:
+            This method reads the JSON file specified by __file_path attribute and deserializes its contents to load objects into the __objects dictionary.
+        """
         try:
             with open(self.__file_path, "r", encoding="utf-8") as file:
-                 load_obj = json.load(file)
-                 for key, value in load_obj.items():
+                load_obj = json.load(file)
+                for key, value in load_obj.items():
                     class_name = value['__class__']
                     obj = globals()[class_name](**value)
                     self.__objects[key] = obj
