236db796a856ce85f3fa4b2cc132f30da2eb28cf
papuskoo <kelvinnayman@gmail.com>
2024-03-11 02:15:22 +0300

    test place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,249 +1,250 @@
 #!/usr/bin/python3
-"""Defines unittests for models/place.py.
 
-Unittest classes:
-    TestPlace_instantiation
-    TestPlace_save
-    TestPlace_to_dict
-"""
-import os
-import models
-import unittest
-from datetime import datetime
-from time import sleep
+import cmd
+import shlex
+from models.base_model import BaseModel
+from models import storage
+from models.user import User
+from models.city import City
+from models.amenity import Amenity
 from models.place import Place
+from models.review import Review
 
 
-class TestPlace_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Place class."""
+class HBNBCommand(cmd.Cmd):
+    """
+    Console interface for managing instances of various classes.
+    """
 
-    def test_no_args_instantiates(self):
-        self.assertEqual(Place, type(Place()))
+    # Prompt for the console
+    prompt = "(hbnb) "
 
-    def test_new_instance_stored_in_objects(self):
-        self.assertIn(Place(), models.storage.all().values())
+    # List of valid class names
+    class_names = ['BaseModel', 'User', 'City', 'Amenity', 'Place', 'Review', 'State']
 
-    def test_id_is_public_str(self):
-        self.assertEqual(str, type(Place().id))
+    def do_EOF(self, line):
+        """
+        Handles the end of file input.
+        """
+        print()
+        return True
 
-    def test_created_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(Place().created_at))
+    def do_quit(self, arg):
+        """
+        Quits the program.
+        """
+        return True
 
-    def test_updated_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(Place().updated_at))
+    def emptyline(self):
+        """
+        Handles an empty line input.
+        """
+        self.lastcmd = ''
 
-    def test_city_id_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(str, type(Place.city_id))
-        self.assertIn("city_id", dir(pl))
-        self.assertNotIn("city_id", pl.__dict__)
+    def do_create(self, arg):
+        """
+        Creates a new instance for the specified class.
 
-    def test_user_id_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(str, type(Place.user_id))
-        self.assertIn("user_id", dir(pl))
-        self.assertNotIn("user_id", pl.__dict__)
+        Syntax:
+            create <class_name>
 
-    def test_name_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(str, type(Place.name))
-        self.assertIn("name", dir(pl))
-        self.assertNotIn("name", pl.__dict__)
+        Example:
+            create BaseModel
+        """
+        # Split the argument into a list of tokens
+        command = shlex.split(arg)
+        if len(command) == 0:
+            print('** class name missing **')
+        elif command[0] not in self.class_names:
+            print("** class doesn't exist **")
+        else:
+            # Dynamically create an instance of the specified class
+            new_instance = eval(f"{command[0]}()")
+            # Save the instance to the storage
+            storage.save()
+            print(new_instance.id)
 
-    def test_description_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(str, type(Place.description))
-        self.assertIn("description", dir(pl))
-        self.assertNotIn("desctiption", pl.__dict__)
+    def do_show(self, arg):
+        """
+        Shows the string representation of an instance.
 
-    def test_number_rooms_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(int, type(Place.number_rooms))
-        self.assertIn("number_rooms", dir(pl))
-        self.assertNotIn("number_rooms", pl.__dict__)
+        Syntax:
+            show <class_name> <instance_id>
 
-    def test_number_bathrooms_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(int, type(Place.number_bathrooms))
-        self.assertIn("number_bathrooms", dir(pl))
-        self.assertNotIn("number_bathrooms", pl.__dict__)
+        Example:
+            show BaseModel 1234-5678-9012
+        """
+        command = shlex.split(arg)
+        if len(command) == 0:
+            print("** class name missing **")
+        elif command[0] not in self.class_names:
+            print("** class name doesn't exist **")
+        elif len(command) < 2:
+            print("** instance id missing **")
+        else:
+            # Retrieve all objects from storage
+            objects = storage.all()
+            # Construct the key for the specified instance
+            key = "{}.{}".format(command[0], command[1])
+            if key in objects:
+                # Print the string representation of the instance
+                print(objects[key])
+            else:
+                print("** no instance found **")
 
-    def test_max_guest_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(int, type(Place.max_guest))
-        self.assertIn("max_guest", dir(pl))
-        self.assertNotIn("max_guest", pl.__dict__)
+    def do_destroy(self, arg):
+        """
+        Deletes a class or an instance of a class.
 
-    def test_price_by_night_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(int, type(Place.price_by_night))
-        self.assertIn("price_by_night", dir(pl))
-        self.assertNotIn("price_by_night", pl.__dict__)
+        Syntax:
+            destroy <class_name> <instance_id>
 
-    def test_latitude_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(float, type(Place.latitude))
-        self.assertIn("latitude", dir(pl))
-        self.assertNotIn("latitude", pl.__dict__)
+        Example:
+            destroy BaseModel 1234-5678-9012
+        """
+        command = shlex.split(arg)
 
-    def test_longitude_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(float, type(Place.longitude))
-        self.assertIn("longitude", dir(pl))
-        self.assertNotIn("longitude", pl.__dict__)
+        if len(command) == 0:
+            print('** class name missing **')
+        elif command[0] not in self.class_names:
+            print("** class doesn't exit **")
+        elif len(command) < 2:
+            print("** instance id is missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(command[0], command[1])
+            if key in objects:
+                # Delete the specified instance from storage
+                del objects[key]
+                storage.save()
+            else:
+                print("** no instance found **")
 
-    def test_amenity_ids_is_public_class_attribute(self):
-        pl = Place()
-        self.assertEqual(list, type(Place.amenity_ids))
-        self.assertIn("amenity_ids", dir(pl))
-        self.assertNotIn("amenity_ids", pl.__dict__)
+    def do_all(self, arg):
+        """
+        Prints the string representation of all instances or specific class.
 
-    def test_two_places_unique_ids(self):
-        pl1 = Place()
-        pl2 = Place()
-        self.assertNotEqual(pl1.id, pl2.id)
+        Syntax:
+            all [<class_name>]
 
-    def test_two_places_different_created_at(self):
-        pl1 = Place()
-        sleep(0.05)
-        pl2 = Place()
-        self.assertLess(pl1.created_at, pl2.created_at)
+        Example:
+            all
+            all BaseModel
+        """
+        objects = storage.all()
+        command = shlex.split(arg)
+        if len(command) == 0:
+            # If no class name is provided, print all instances
+            for key, value in objects.items():
+                print(str(value))
+        elif command[0] not in self.class_names:
+            print("** class doesn't exist **")
+        else:
+            # If class name is provided, print instances of that class
+            for key, value in objects.items():
+                if key.split(".")[0] == command[0]:
+                    print(str(value))
 
-    def test_two_places_different_updated_at(self):
-        pl1 = Place()
-        sleep(0.05)
-        pl2 = Place()
-        self.assertLess(pl1.updated_at, pl2.updated_at)
+    def default(self, arg):
+        """
+        Handles commands on the command line.
 
-    def test_str_representation(self):
-        dt = datetime.today()
-        dt_repr = repr(dt)
-        pl = Place()
-        pl.id = "123456"
-        pl.created_at = pl.updated_at = dt
-        plstr = pl.__str__()
-        self.assertIn("[Place] (123456)", plstr)
-        self.assertIn("'id': '123456'", plstr)
-        self.assertIn("'created_at': " + dt_repr, plstr)
-        self.assertIn("'updated_at': " + dt_repr, plstr)
+        This method is invoked for any command not recognized by other do_* methods.
+        It tries to interpret the command in the form '<class_name>.<method_name>(<arguments>)'.
+        If the method name is recognized, it delegates the execution to the corresponding method.
+        Otherwise, it prints an error message.
+        """
+        arg_list = arg.split(".")
+        new_class_name = arg_list[0]
+        command = arg_list[1].split('(')
 
-    def test_args_unused(self):
-        pl = Place(None)
-        self.assertNotIn(None, pl.__dict__.values())
+        new_method = command[0]
+        extra_arg = command[1].split('(')[0]
 
-    def test_instantiation_with_kwargs(self):
-        dt = datetime.today()
-        dt_iso = dt.isoformat()
-        pl = Place(id="345", created_at=dt_iso, updated_at=dt_iso)
-        self.assertEqual(pl.id, "345")
-        self.assertEqual(pl.created_at, dt)
-        self.assertEqual(pl.updated_at, dt)
-
-    def test_instantiation_with_None_kwargs(self):
-        with self.assertRaises(TypeError):
-            Place(id=None, created_at=None, updated_at=None)
-
-
-class TestPlace_save(unittest.TestCase):
-    """Unittests for testing save method of the Place class."""
-
-    @classmethod
-    def setUp(self):
-        try:
-            os.rename("file.json", "tmp")
-        except IOError:
-            pass
-
-    def tearDown(self):
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-        try:
-            os.rename("tmp", "file.json")
-        except IOError:
-            pass
-
-    def test_one_save(self):
-        pl = Place()
-        sleep(0.05)
-        first_updated_at = pl.updated_at
-        pl.save()
-        self.assertLess(first_updated_at, pl.updated_at)
-
-    def test_two_saves(self):
-        pl = Place()
-        sleep(0.05)
-        first_updated_at = pl.updated_at
-        pl.save()
-        second_updated_at = pl.updated_at
-        self.assertLess(first_updated_at, second_updated_at)
-        sleep(0.05)
-        pl.save()
-        self.assertLess(second_updated_at, pl.updated_at)
-
-    def test_save_with_arg(self):
-        pl = Place()
-        with self.assertRaises(TypeError):
-            pl.save(None)
-
-    def test_save_updates_file(self):
-        pl = Place()
-        pl.save()
-        plid = "Place." + pl.id
-        with open("file.json", "r") as f:
-            self.assertIn(plid, f.read())
-
-
-class TestPlace_to_dict(unittest.TestCase):
-    """Unittests for testing to_dict method of the Place class."""
-
-    def test_to_dict_type(self):
-        self.assertTrue(dict, type(Place().to_dict()))
-
-    def test_to_dict_contains_correct_keys(self):
-        pl = Place()
-        self.assertIn("id", pl.to_dict())
-        self.assertIn("created_at", pl.to_dict())
-        self.assertIn("updated_at", pl.to_dict())
-        self.assertIn("__class__", pl.to_dict())
-
-    def test_to_dict_contains_added_attributes(self):
-        pl = Place()
-        pl.middle_name = "Holberton"
-        pl.my_number = 98
-        self.assertEqual("Holberton", pl.middle_name)
-        self.assertIn("my_number", pl.to_dict())
-
-    def test_to_dict_datetime_attributes_are_strs(self):
-        pl = Place()
-        pl_dict = pl.to_dict()
-        self.assertEqual(str, type(pl_dict["id"]))
-        self.assertEqual(str, type(pl_dict["created_at"]))
-        self.assertEqual(str, type(pl_dict["updated_at"]))
-
-    def test_to_dict_output(self):
-        dt = datetime.today()
-        pl = Place()
-        pl.id = "123456"
-        pl.created_at = pl.updated_at = dt
-        tdict = {
-            'id': '123456',
-            '__class__': 'Place',
-            'created_at': dt.isoformat(),
-            'updated_at': dt.isoformat(),
+        method_dict = {
+            'all': self.do_all,
+            'show': self.do_show,
+            'destroy': self.do_destroy,
+            'update': self.do_update,
+            'count': self.do_count
         }
-        self.assertDictEqual(pl.to_dict(), tdict)
+        if new_method in method_dict.keys():
+            return method_dict[new_method]("{} {}".format(new_class_name, extra_arg))
 
-    def test_contrast_to_dict_dunder_dict(self):
-        pl = Place()
-        self.assertNotEqual(pl.to_dict(), pl.__dict__)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
-    def test_to_dict_with_arg(self):
-        pl = Place()
-        with self.assertRaises(TypeError):
-            pl.to_dict(None)
+    def do_count(self, arg):
+        """
+        Counts the number of instances.
 
+        Syntax:
+            count <class_name>
 
-if __name__ == "__main__":
-    unittest.main()
+        Example:
+            count BaseModel
+        """
+        objects = storage.all()
+
+        command = shlex.split(arg)
+        new_class_name = command[0]
+
+        count = 0
+        if command:
+            if new_class_name in self.class_names:
+                for obj in objects.values():
+                    if obj.__class__.__name__ == new_class_name:
+                        count += 1
+                print(count)
+            else:
+                print("** invalid class name **")
+        else:
+            print("** class name missing **")
+
+    def do_update(self, arg):
+        """
+        Updates an instance by adding or updating an attribute.
+
+        Syntax:
+            update <class_name> <instance_id> <attribute_name> <attribute_value>
+
+        Example:
+            update BaseModel 1234-5678-9012 name "New Name"
+        """
+        command = shlex.split(arg)
+        if len(command) == 0:
+            print("** class name missing **")
+        elif command[0] not in self.class_names:
+            print("** class doesn't exist **")
+        elif len(command) < 2:
+            print("** instance is missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(command[0], command[1])
+            if key not in objects:
+                print("** no instance found **")
+            elif len(command) < 3:
+                print("** attribute name missing **")
+            elif len(command) < 4:
+                print("** value missing **")
+            else:
+                obj = objects[key]
+                attr_name = command[2]
+                attr_val = command[3]
+
+                try:
+                    attr_val = eval(attr_val)
+                except Exception:
+                    pass
+                setattr(obj, attr_name, attr_val)
+
+                obj.save()
+
+    def onecmd(self, line):
+        """
+        Handles execution on the interface.
+        """
+        return cmd.Cmd.onecmd(self, line)
+
+if __name__ == '__main__':
+    HBNBCommand().cmd
